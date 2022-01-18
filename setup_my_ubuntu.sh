614d06c6b08cf9b662b3c3173d3dc11563467b0e
tanmoy7989 <tanmoy.7989@gmail.com>
2022-01-17 22:05:34 -0800

    addd linux setup

--- setup_my_ubuntu.sh
+++ setup_my_ubuntu.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 # This script assumes the following:
-# - you have root access to the macbook you are trying to set up.
-# - the OS version is Big Sur or higher.
+# - you have root access to the linux box you are trying to set up.
+# - the OS is Ubuntu 16.04+ or higher.
+# - the Snap package manager is already installed.
 # - <others>
 
 # Tanmoy Sanyal
 
 # bashrc
 cp bashrc $HOME/.bashrc
-# hide pesky zsh message (zsh is now the default shell, but long time bash # user here)
-echo "export BASH_SILENCE_DEPRECATION_WARNING=1" >> $HOME/.bash_profile
 
 # set path for downloading softwares
 SOFTWARE_DOWNLOAD_PATH=$HOME/mysoftware
@@ -24,8 +23,8 @@ echo -e "#conda\nsource $MINICONDA_INSTALL_PATH/etc/profile.d/conda.sh" >> $HOME
 echo "conda activate" >> $HOME/.bashrc
 source $HOME/.bashrc
 
-# git (through command-line tools)
-xcode-select --install
+# git
+sudo apt install git 
 
 # git config
 git config --global user.name "tanmoy7989"
@@ -47,6 +46,8 @@ echo "Press any key to continue"
 read
 
 # vim
+sudo apt install vim
+
 mkdir -p $HOME/.vim/bundle
 git clone git@github.com:VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 cp vimrc $HOME/.vim/vimrc
@@ -65,11 +66,47 @@ conda env create --file scienv.yml
 # clear conda cache
 conda clean -y -t
 
-echo "If you this is a new macbook, also download the following using the given links:"
-echo "Install Brave browser from: https://brave.com/download/"
-echo "Install Zoom from : https://zoom.us/download"
-echo "Install Microsoft teams from: https://www.microsoft.com/en-us/microsoft-teams/download-app"
-echo "Install Slack from: https://apps.apple.com/app/slack/id803453959"
-echo "Install VSCode from: https://code.visualstudio.com/docs?dv=osx"
-echo "Install iterm from https://iterm2.com/downloads.html"
+# sshfs
+sudo apt install sshfs
 
+# latex
+echo -e "\nInstalling latex... (please be patient)"
+sudo apt install texlive-latex-extra
+sudo apt install texmaker
+
+# brave browser (privacy and anti-tracking out-of-the-box)
+# instructions at: https://brave-browser.readthedocs.io/en/latest/installing-brave.html
+sudo apt install apt-transport-https curl
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser
+
+# zoom
+wget https://zoom.us/client/latest/zoom_amd64.deb
+chmod +x zoom_amd64.deb
+sudo apt install ./zoom_amd64.deb
+rm zoom_amd64.deb
+
+# slack
+sudo snap install slack --classic
+
+# vscode (TODO: test this)
+# sudo snap install vscode
+
+echo "If you this is a new linux box, exit this script (Ctrl + z) and also download the following using the given links:"
+echo "Install Microsoft teams from: https://www.microsoft.com/en-us/microsoft-teams/download-app"
+read
+
+# privacy (prevent Canonical from snooping in on you)
+# source: https://securitronlinux.com/ubuntu-2/how-to-help-stop-ubuntu-spying-on-you/
+ubuntu-report -f send no
+sudo apt purge popularity-contest whoopsie
+sudo vim /etc/default/motd-news # set ENABLED=0 in this file
+gsettings set com.ubuntu.geoip geoip-url ""
+
+# refresh
+sudo apt upgrade && sudo apt update
+sudo apt autoremove
+sudo apt autoclean
+sudo reboot now
--- vimrc
+++ vimrc
@@ -1,29 +1,77 @@
-set shell=/bin/bash
+"required
 set nocompatible
+filetype off
 
-if has('filetype')
-    filetype indent plugin on
-endif
+"bash-shell (in case you ain't using it)
+set shell=/bin/bash
 
-if has('syntax')
-    syntax on
-endif
+"line numbering
+set nu
 
-set number
-set ruler
+"no swap files
+set noswapfile
 
-set autoindent 
-set expandtab
+"utf-8 support
+set encoding=utf-8
+
+"put all plugins under my vim bundle
+set rtp+=~/.vim/bundle/Vundle.vim
+
+"use vundle to manage all (most) plugins
+call vundle#begin()
+Plugin 'gmarik/Vundle.vim'
+
+"filesystem view
+Plugin 'scrooloose/nerdtree'
+Plugin 'jistr/vim-nerdtree-tabs'
+Plugin 'kien/ctrlp.vim'
+
+"code-folding
+Plugin 'tmhedberg/SimpylFold'
+
+"python sytax checker
+Plugin 'nvie/vim-flake8'
+Plugin 'vim-scripts/Pydiction'
+Plugin 'vim-scripts/indentpython.vim'
+Plugin 'scrooloose/syntastic'
+
+"python autocomplete (doesnt work so well on my mac so letting it go for now)
+"Plugin 'davidhalter/jedi-vim'
+
+"colors (mustang)
+Plugin 'croaker/mustang-vim'
+
+call vundle#end()
+
+"required (now)
+filetype plugin indent on
+
+"follow-ups to the above plugins
+"don't want to see .pyc files
+let NERDTreeIgnore=['\.pyc$', '\~$'] "ignore files in NERDTree
+
+"code folding
+set foldmethod=indent
+let g:SimpylFold_docstring_preview = 1
+
+"auto-complete window
+let g:ycm_autoclose_preview_window_after_completion=1
+
+"set color
+colorscheme desert
+
+"PEP-8 standards"
+"I'm gonna stick to this all-over because I use python the most
 set tabstop=4
 set softtabstop=4
 set shiftwidth=4
 set textwidth=79
+set expandtab
+set autoindent
+set fileformat=unix
 
-set wildmenu
-set showcmd
-set encoding=utf-8
+"python-highlighting
+let python_highlight_all=1
+syntax on
 
-set noswapfile
-set fileformats=mac
-
-set visualbell
+"lang-specific settings other than python
